35662989a7d4071afc6409c6979e9708f32b6d79
SahuParul <158465364+SahuParul@users.noreply.github.com>
2024-07-27 19:52:06 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import ProductTab from './ProductTab.jsx';
+
+function App() {
+  return (
+    <>
+    <h3>Blockbuster Deals! Shop Now</h3>
+    <ProductTab/>
+    </>
+  );
+}
+
+export default App;
